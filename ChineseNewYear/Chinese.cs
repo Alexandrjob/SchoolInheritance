5f4829a7f80acf889a9bdd0f411d9a08dd1118e8
Alexandr <60702539+Alexandrjob@users.noreply.github.com>
2020-02-07 21:41:25 +1100

    Заморочки

---
+++ ChineseNewYear/Chinese.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChineseNewYear
+{
+    sealed class Chinese
+    {
+        private int eyeWidth;
+        private int percentageOfImmunity;
+        private bool isDead;
+        private bool isinFected;
+        private bool isMaskDressed;
+
+        public Chinese(int percentageOfImmunity = 70,bool isMaskDressed = false,bool isDead = false,bool isInFected = false)
+        {
+            this.percentageOfImmunity = percentageOfImmunity;
+            this.isDead = isDead;
+            this.isinFected = isInFected;
+            this.isMaskDressed = isMaskDressed;
+        }
+
+        public int SetPercentageOfImmunity
+        {
+            get { return percentageOfImmunity; }
+            set
+            {
+                percentageOfImmunity = value;
+                if(percentageOfImmunity <= 0)
+                {
+                    isDead = true;
+                }
+                if(percentageOfImmunity <= 30)
+                {
+                    isinFected = true;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string myState;
+            myState = string.Format("[Immunity: {0}%; isMaskDressed: {1}; isInfected: {2}; isDead: {3} ]", percentageOfImmunity, isMaskDressed, isinFected, isDead);
+            return myState;
+        }
+    }
+}
---
+++ ChineseNewYear/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChineseNewYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Chinese chineseMan = new Chinese(80, true, false, false);
+            
+            Console.WriteLine(chineseMan);
+            //Console.Read();
+        }
+    }
+}
