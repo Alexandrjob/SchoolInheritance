2db1e49ca571bdb0f3a4b3d9c1b96a38c7526673
Alexandr <60702539+Alexandrjob@users.noreply.github.com>
2020-02-07 23:01:19 +1100

    Реализовал жизненный цикл

--- ChineseNewYear/Program.cs
+++ ChineseNewYear/Program.cs
@@ -1,17 +1,47 @@
 ﻿using System;
+using System.Timers;
+using Timers = System.Timers;
 
 namespace ChineseNewYear
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        //private static Timer _timer;
+        private static Timers.Timer _timer;
 
-            Chinese chineseMan = new Chinese(80, true, false, false);
-            
-            Console.WriteLine(chineseMan);
+        public static void Main(string[] args)
+        {
+            /*_timer = new Timers.Timer
+            {
+                AutoReset = true,
+                Interval = 1000,
+                Enabled = true
+            };
+            _timer.Elapsed += LifeCycle;
+            _timer.Start();
+            */
+            using (Timer timer = new Timer())
+            {
+                timer.Interval = 2000;//интервал задается в миллисекундах
+                timer.Elapsed += LifeCycle;
+                timer.AutoReset = true;
+                timer.Start();
+                //Ставим основной поток на ожидание, т.к. таймер исполняется в 
+                //отдельном потоке и не препятствует завершению основного потока.
+                //Console.ReadLine();
+            }
+
+            //Chinese chineseMan = new Chinese(80, true, false, false);
+
+            //Console.WriteLine(chineseMan);
             //Console.Read();
+        }
+
+        private static void LifeCycle(object source, ElapsedEventArgs e)
+        {
+            Chinese chineseMan = new Chinese(80, true, false, false);
+
+            Console.WriteLine(chineseMan);
         }
     }
 }
